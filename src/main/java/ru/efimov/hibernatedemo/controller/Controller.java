731a729a7e88e76ae2790d3daa4fb549ec80e097
Neto test <ehimovsergei@mail.ru>
2024-03-19 17:33:10 +0300

    jpa-repository added

--- src/main/java/ru/efimov/hibernatedemo/controller/Controller.java
+++ src/main/java/ru/efimov/hibernatedemo/controller/Controller.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import ru.efimov.hibernatedemo.entity.Person;
 import ru.efimov.hibernatedemo.repository.HibRepository;
 import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor
 @RestController
@@ -16,6 +17,14 @@ public class Controller {
 
     @GetMapping("/persons/by-city")
         public List<Person> getPersons(@RequestParam String city){
-        return repository.getPersonsByCity(city);
+        return repository.findByCityOfLiving(city);
+    }
+    @GetMapping("/persons/by-age")
+    public List<Person> findByAgeLessThanOrderByAge(@RequestParam int age){
+        return repository.findByAgeLessThanOrderByAge(age);
+    }
+    @GetMapping("/persons/by-name-and-surname")
+    public Optional<Person> findByNameAndSurname(@RequestParam String name, @RequestParam String surname){
+        return repository.findByNameAndSurname(name,surname);
     }
 }
--- src/main/java/ru/efimov/hibernatedemo/repository/HibRepository.java
+++ src/main/java/ru/efimov/hibernatedemo/repository/HibRepository.java
@@ -1,19 +1,19 @@
 package ru.efimov.hibernatedemo.repository;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.efimov.hibernatedemo.entity.Person;
 
 import java.util.List;
+import java.util.Optional;
+
 
 @Repository
-public class HibRepository {
+public interface HibRepository extends JpaRepository<Person, Integer> {
 
-    @PersistenceContext
-    private EntityManager entityManager;
+    List<Person> findByCityOfLiving(String city);
 
-    public List<Person> getPersonsByCity(String city) {
-        return entityManager.createQuery("select p from Person p where p.cityOfLiving = :city order by p.cityOfLiving", Person.class).setParameter("city", city).getResultList();
-    }
+    List<Person> findByAgeLessThanOrderByAge(int age);
+
+    Optional<Person> findByNameAndSurname(String name, String surname);
 }
